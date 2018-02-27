e8eb568107903473affc48883df2f4be16f5e983
Yorann <yorannb@hotmail.fr>
2018-02-27 17:33:26 +0100

    clean

--- src/main/java/poe/spring/api/GreetingController.java
+++ src/main/java/poe/spring/api/GreetingController.java
@@ -12,12 +12,12 @@ import poe.spring.domain.Greeting;
 
 @RestController
 public class GreetingController {
-	private static final String template = "Hello, %s!";
+	private static final String TEMPLATE = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@RequestMapping("/hello")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
 	}
 
 	
