5ee540e2a78a57d144bd693a4f82d97f639a7464
Yorann <yorannb@hotmail.fr>
2018-02-28 11:09:42 +0100

    ajout css

--- src/main/java/poe/spring/Application.java
+++ src/main/java/poe/spring/Application.java
@@ -1,11 +1,24 @@
 package poe.spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class Application {
+	// nom de la classe qui log
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+	
 	 public static void main(String[] args) {
 	        SpringApplication.run(Application.class, args);
+
+	        logger.info("info from log");
+	        logger.debug("debug from log");
+	        logger.error("error from log");
+	        logger.warn("warn from log");
+	        logger.trace("trace from log");
 	    }
 }
